f6766be21ff3cc06c7901d9b52dc3a31fab71829
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-16 16:15:22 +0200

    add search input as a pokemon finder

--- pokedex.js
+++ pokedex.js
@@ -24,59 +24,52 @@ function getOnePokemon(url) {
 }
 const search$$ = document.querySelector(".buscador");
 
-const buscar = (pokemons) => {
-
-  console.log(search$$.value);
-  const pokemonsFiltered = [];
-  for (const pokemon of pokemons) {
-    if (pokemon.name.includes(search$$.value)) {
-      pokemonsFiltered.push(pokemon);
-    }
-  }
-  renderPokemons(pokemonsFiltred);
-
-};
-
-// function printFinder(param){
-//     const divFinder$$ = document.createElement('div');
-//     const p$$ = document.createElement('p');
-//     const input$$ = document.createElement('input')
-
-//     divFinder$$.classList.add('divFinder');
-//     input$$.setAttribute('type', 'text');
-//     p$$.textContent = 'Wanna find specifically ?';
-//     divFinder$$.appendChild(p$$);
-//     divFinder$$.appendChild(input$$);
-
-//     const h1$$ = document.querySelector('h1');
-
-//     h1$$.appendChild(divFinder$$);
-
-//     input$$.addEventListener('input', buscar())
-// }
-// const buscar = (pokemons) =>{
-
-//     const pokemonsFiltered = [];
-
-//     for(const poke of ALL_POKEMONS_INFO){
-//         console.log(poke);
-//         if(poke.name.includes(input$$.value)){
-//             pokemonsFiltered.push(poke)
-//         }
+// const buscar = (pokemons) => {
+//   console.log(search$$.value);
+//   const pokemonsFiltered = [];
+//   for (const pokemon of pokemons) {
+//     if (pokemon.name.includes(search$$.value)) {
+//       pokemonsFiltered.push(pokemon);
 //     }
-//     renderPokemons(pokemonsFiltered)
-// }
+//   }
+//   renderPokemons(pokemonsFiltred);
+// };
+const input$$ = document.createElement('input')
+function  renderSearch(pokemons){
+    const divFinder$$ = document.createElement('div');
+    const p$$ = document.createElement('p');
+
+    divFinder$$.classList.add('divFinder');
+    input$$.setAttribute('type', 'text');
+    p$$.textContent = 'Wanna find specifically ?';
+    divFinder$$.appendChild(p$$);
+    divFinder$$.appendChild(input$$);
+
+    const h1$$ = document.querySelector('h1');
+
+    h1$$.appendChild(divFinder$$);
+
+}
+const buscar = (event) =>{
+    console.log(event);
+    const inputValue = event.target.value.toLowerCase();
+    const pokemonsFiltered = ALL_POKEMONS_INFO.filter((pokemon) => {
+        const match = pokemon.name.toLowerCase().includes(inputValue)
+        return match
+    }) 
+
+   renderPokemons(pokemonsFiltered)
+}
 
 let audioDiv = document.createElement("div");
 
 function renderPokemons(pokemons) {
-    pokedex$$.innerHTML = '';
+  pokedex$$.innerHTML = "";
 
   for (const poke of pokemons) {
     const li$$ = document.createElement("li");
     li$$.classList.add("card");
-    // const pokeHtml =
-    // `img src=${poke.sprites.front_default} alt=${poke.name} `
+
     const img$$ = document.createElement("img");
     img$$.classList.add("card-image");
     img$$.src = poke.sprites.front_default;
@@ -89,41 +82,13 @@ function renderPokemons(pokemons) {
     const div$$ = document.createElement("div");
     div$$.classList.add("card-subtitle");
 
-    if (poke.types[0].type.name === "grass") {
-      li$$.classList.add("grass");
-    } else if (poke.types[0].type.name === "fire") {
-      li$$.classList.add("fire");
-    } else if (poke.types[0].type.name === "water") {
-      li$$.classList.add("water");
-    } else if (poke.types[0].type.name === "bug") {
-      li$$.classList.add("bug");
-    } else if (poke.types[0].type.name === "normal") {
-      li$$.classList.add("normal");
-    } else if (poke.types[0].type.name === "poison") {
-      li$$.classList.add("poison");
-    } else if (poke.types[0].type.name === "electric") {
-      li$$.classList.add("electric");
-    } else if (poke.types[0].type.name === "ground") {
-      li$$.classList.add("ground");
-    } else if (poke.types[0].type.name === "fairy") {
-      li$$.classList.add("fairy");
-    } else if (poke.types[0].type.name === "fighting") {
-      li$$.classList.add("fighting");
-    } else if (poke.types[0].type.name === "psychic") {
-      li$$.classList.add("psychic");
-    } else if (poke.types[0].type.name === "rock") {
-      li$$.classList.add("rock");
-    } else if (poke.types[0].type.name === "ghost") {
-      li$$.classList.add("ghost");
-    } else if (poke.types[0].type.name === "ice") {
-      li$$.classList.add("ice");
-    } else if (poke.types[0].type.name === "dragon") {
-      li$$.classList.add("dragon");
-    }
+    li$$.classList.add(poke.types[0].type.name)
 
     li$$.appendChild(img$$);
     li$$.appendChild(p$$);
     li$$.appendChild(div$$);
+    
+    pokedex$$.appendChild(li$$);
 
     const cries = "https://play.pokemonshowdown.com/audio/cries/src/" + poke.name + ".wav";
     li$$.addEventListener("click", () => {
@@ -132,11 +97,9 @@ function renderPokemons(pokemons) {
       </audio>`;
       document.body.appendChild(audioDiv);
     });
-    pokedex$$.appendChild(li$$);
+
+    
   }
-//   pokemons.forEach(function (poke) {
-   
-//   });
 }
 
 //Director de orquesta, irá llamando a otras funciones
@@ -159,20 +122,9 @@ async function init() {
   console.log("ALL_POKEMONS_INFO", ALL_POKEMONS_INFO);
 
   renderPokemons(ALL_POKEMONS_INFO);
-  //   printFinder()
+  renderSearch(ALL_POKEMONS_INFO)
 
-  search$$.addEventListener("input", () => buscar(allPokemons));
+  input$$.addEventListener("input", () => buscar(event));
 }
 window.onload = init;
 
-// for (let i = 0; i < 150; i++) {
-//     console.log('https://pokeapi.co/api/v2/pokemon/' + index);
-
-// }
-// for (let i = 1; i < 152; i++) {
-
-//     fetch("https://pokeapi.co/api/v2/pokemon/"+i)
-
-//     .then((characters) => characters.json())
-
-//     .then((characters) => console.log(characters));
