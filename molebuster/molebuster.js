c0ac97c27051ae05fe472644336f807deb5db572
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-18 20:13:00 +0200

    create interval of images change

--- molebuster/molebuster.js
+++ molebuster/molebuster.js
@@ -1,7 +1,48 @@
 const gameBox$$ = document.querySelector('.game--container');
 const playButton$$ = document.querySelector('button');
 const holeImg$$ = "../molebuster/dirt.png";
+const diggletImg$$ = "../molebuster/digglet.png";
 let score$$ = 0;
+
+const play = () => {
+    const findHoles$$ = document.querySelectorAll('.hole');
+    console.log(findHoles$$);
+    
+    let timeOut = 1000;
+    let before = 0;
+
+    for (let i = 30; i >= 0 ; i--) {
+        let random = Math.floor(Math.random()*9);
+        setTimeout(() => {
+            if(i != 0){
+                const count$$ = document.querySelector('.count');
+                findHoles$$[before].setAttribute('src', holeImg$$);
+                findHoles$$[before].classList.replace('holes', 'digglet');
+                
+                findHoles$$[random].setAttribute('src', diggletImg$$);
+                findHoles$$[random].classList.replace('holes', 'digglet');
+                before = random;
+            if(i<31){
+                count$$.textContent = `00:${i}`;
+                count$$.style = "color: red";
+            }
+            if(i<10){
+                count$$.textContent = `00:0${i}`;
+                count$$.style = "color: red";
+            }if(i === 0){
+                count$$.textContent = `00:00`;
+                count$$.style = "color: red";
+            }
+            }
+
+        }, timeOut)
+
+        timeOut += 1000;
+        
+    }
+
+
+}
 
 const renderGame = () =>{
     gameBox$$.innerHTML = "<div class='game'></div>";
