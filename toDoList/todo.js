ccca7af601178c8f43e966482f779b278ea35258
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-19 10:12:37 +0200

    add text word by word effect in to do list

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -3,7 +3,12 @@ console.log("Js funcionando");
 const h1$$ = document.querySelector("h1");
 const box$$ = document.getElementById("box");
 
-const divForToDoList$$ = document.querySelector(".divForToDoList");
+const para$$ = document.querySelector(".description");
+const nurseDiv$$ = document.createElement('img');
+nurseDiv$$.classList.add('nursediv')
+nurseDiv$$.src = "./Cute Nurse Joy Kandi Pattern.png"
+box$$.insertBefore(nurseDiv$$,para$$)
+
 
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
@@ -26,3 +31,28 @@ document.querySelector("#push").onclick = function () {
     }
   }
 };
+
+
+// FUNCTION WORD BY WORD. BY GOOGLE HAHAHA
+
+function write(contenedor,writer,speed){
+
+   longitud = writer.length;
+
+   cnt = document.getElementById(contenedor);
+   let i=0;
+   tiempo = setInterval(function(){
+      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + writer.charAt(i)+ " ";
+      if(i >= longitud){
+         clearInterval(tiempo);
+         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
+         return true;
+      } else {
+         i++;
+      }},speed);
+};
+
+let texto =  "Bienvenido entrenador Pokemón. En esta sección podrás crear tu propia To Do List. Para ser un buen entrenador y un buen programador, debes organizarte bien. Así que ahora puedes utilizar esta herramienta para añadir tus tareas por hacer; o todos los accesorios que necesitas tener andes de salir a cazar pokemons. Una vez los tengas, podrás eliminarlos. ";
+$("#write").click(function() {
+write("text",texto,100);
+});
